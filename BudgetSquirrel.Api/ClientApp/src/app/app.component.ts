4919008ea1c95feb35813c67b891b05df3867399
Ben Groseclose <benjamin.groseclose@hotmail.com>
2020-04-01 18:39:35 -0400

    Angular init stuff

--- BudgetSquirrel.Api/ClientApp/src/app/app.component.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "bs-root",
+  template: `
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "budgetsquirrel";
 }
