4e02c683b557bdf046f91a0d6a8e4db211b96597
Ian Kirkpatrick <ikirkpat@oaisd.org>
2020-04-28 21:17:38 -0400

    Make form look good

--- BudgetSquirrel.Api/ClientApp/src/app/account/sign-in/sign-in.component.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/account/sign-in/sign-in.component.ts
@@ -9,25 +9,27 @@ import { ROUTES } from 'src/app/route-constants';
 @Component({
   selector: "bs-sign-in",
   template: `
-    <form [formGroup]="this.loginForm" class="sign-in" (ngSubmit)="onSubmit(loginForm)">
-      <h3>Sign In</h3>
-      <mat-form-field>
-        <input name="username" matInput placeholder="Username" formControlName="username">
-        <mat-error *ngIf="usernameValidation.invalid">Please enter your username</mat-error>
-      </mat-form-field>
+    <div class="vert-section">
+      <form [formGroup]="this.loginForm" class="vert-section__content login-page" (ngSubmit)="onSubmit(loginForm)">
+        <h1>Login to Budget Squirrel</h1>
+        <mat-form-field class="form-field__container--block" appearance="fill">
+          <input name="username" matInput placeholder="Username" formControlName="username">
+          <mat-error *ngIf="usernameValidation.invalid">Please enter your username</mat-error>
+        </mat-form-field>
 
-      <mat-form-field>
-        <input name="password" matInput minlength="6" placeholder="Password" autocomplete="off"
-          [type]="(this.hidePassword ? 'password' : 'text')" formControlName="password">
-        <button type="button" mat-icon-button matSuffix (click)="this.hidePassword = !this.hidePassword"
-          [attr.aria-label]="'Hide password'" [attr.aria-pressed]="hide">
-          <mat-icon tabindex="-1">{{this.hidePassword ? 'visibility_off' : 'visibility'}}</mat-icon>
-        </button>
-        <mat-error *ngIf="passwordValidation.invalid">Password must contain at least 6 characters</mat-error>
-      </mat-form-field>
+        <mat-form-field class="form-field__container--block" appearance="fill">
+          <input name="password" matInput minlength="6" placeholder="Password" autocomplete="off"
+            [type]="(this.hidePassword ? 'password' : 'text')" formControlName="password">
+          <button type="button" mat-icon-button matSuffix (click)="this.hidePassword = !this.hidePassword"
+            [attr.aria-label]="'Hide password'" [attr.aria-pressed]="hide">
+            <mat-icon tabindex="-1">{{this.hidePassword ? 'visibility_off' : 'visibility'}}</mat-icon>
+          </button>
+          <mat-error *ngIf="passwordValidation.invalid">Password must contain at least 6 characters</mat-error>
+        </mat-form-field>
 
-      <button mat-flat-button type="submit" color="primary">Submit</button>
-    </form>
+        <button class="login-btn button button--primary button--wide" type="submit">Login</button>
+      </form>
+    </div>
   `,
   styleUrls: ["./sign-in.component.scss"]
 })
@@ -48,6 +50,8 @@ export class SignInComponent implements OnInit {
     this.loginForm = this.formBuilder.group({
       username: this.usernameValidation,
       password: this.passwordValidation
+    }, {
+      updateOn: "submit"
     });
   }
 
