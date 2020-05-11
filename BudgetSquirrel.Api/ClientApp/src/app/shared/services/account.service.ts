dad228a8bc9636f06438f025a64da33b8648bba2
Ben Groseclose <benjamin.groseclose@hotmail.com>
2020-05-11 18:51:27 -0400

    favicon and logout work

--- BudgetSquirrel.Api/ClientApp/src/app/shared/services/account.service.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/shared/services/account.service.ts
@@ -38,11 +38,10 @@ export class AccountService {
     return this.http.delete(`${this.baseUrl}${AUTHENTICATION_API}`).toPromise();
   }
 
-  public logout(): Observable<boolean> {
-    return this.http.get(`${this.baseUrl}${AUTHENTICATION_API}/logout`).pipe(
-      tap((user: User) => this.userSubject.next(EMPTY_USER)),
-      map(() => true)
-    );
+  public logout(): void {
+    this.http.get(`${this.baseUrl}${AUTHENTICATION_API}/logout`).toPromise().then(() => {
+      this.userSubject.next(EMPTY_USER);
+    });
   }
 
   public createUser(newUser: NewUser): Observable<User> {
