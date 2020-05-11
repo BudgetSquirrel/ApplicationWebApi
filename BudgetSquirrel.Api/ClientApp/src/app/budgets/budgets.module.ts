c1b67be4baac1d6c87f84f44a6682929b1a13291
Ben Groseclose <benjamin.groseclose@hotmail.com>
2020-05-11 18:52:56 -0400

    Remove not need component

--- BudgetSquirrel.Api/ClientApp/src/app/budgets/budgets.module.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/budgets/budgets.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from "@angular/common";
 import { BudgetsComponent } from "./budgets/budgets.component";
 import { SharedModule } from "../shared/shared.module";
 import { MatExpansionModule } from "@angular/material";
-import { BudgetDetailsComponent } from './budget-details/budget-details.component';
 
 @NgModule({
   declarations: [
-    BudgetsComponent,
-    BudgetDetailsComponent
+    BudgetsComponent
   ],
   imports: [
     CommonModule,
