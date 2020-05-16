5ad8c75e2dab2870cb968b379d4e774abc090ef0
Ian Kirkpatrick <ikirkpat@oaisd.org>
2020-05-16 14:04:06 -0400

    Refactor migrations

--- BudgetSquirrel.Api/Services/Implementations/AsyncQueryService.cs
+++ BudgetSquirrel.Api/Services/Implementations/AsyncQueryService.cs
@@ -4,11 +4,25 @@ using System.Linq.Expressions;
 using System.Threading.Tasks;
 using BudgetSquirrel.Business;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace BudgetSquirrel.Api.Services.Implementations
 {
   public class AsyncQueryService : IAsyncQueryService
   {
+    public IQueryable<T> Include<T, TProperty>(IQueryable<T> source, Expression<Func<T, TProperty>> include) where T : class
+    {
+      if (source is DbSet<T>)
+      {
+        DbSet<T> includable = ((DbSet<T>) source);
+        return includable.Include(include);
+      }
+      else
+      {
+        throw new InvalidOperationException("source must be a DbSet, not " + source.GetType().Name);
+      }
+    }
+
     public Task<T> SingleOrDefaultAsync<T>(IQueryable<T> source, Expression<Func<T, bool>> predicate)
     {
       return source.SingleOrDefaultAsync(predicate);
