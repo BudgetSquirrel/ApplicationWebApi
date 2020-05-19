943768148a489d5cbc8afb93df4fe8b78369c581
Ian Kirkpatrick <ikirkpat@oaisd.org>
2020-05-18 20:35:31 -0400

    Add inplace input styles

--- BudgetSquirrel.Api/ClientApp/src/app/budgets/budgets/budgets.component.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/budgets/budgets/budgets.component.ts
@@ -12,11 +12,34 @@ export class BudgetsComponent implements OnInit {
 
   public rootBudget: Budget = nullBudget;
 
+  public isEditingRootName: boolean = false;
+  public isEditingRootAmount: boolean = false;
+
   constructor(private budgetService: BudgetService) { }
 
   public ngOnInit() {
     this.budgetService.getRootBudget().subscribe(
       (budget: Budget) => this.rootBudget = budget
     );
+  }
+
+  public onOpenInplaceEdit(field: string, event: MouseEvent) {
+    if (event.button !== 0) {
+      return;
+    }
+
+    if (field == "rootAmount") {
+      this.isEditingRootAmount = true;
+    } else if (field == "rootName") {
+      this.isEditingRootName = true;
+    }
+  }
+
+  public onBlurInplaceEdit(field: string, event: MouseEvent) {
+    if (field == "rootAmount") {
+      this.isEditingRootAmount = false;
+    } else if (field == "rootName") {
+      this.isEditingRootName = false;
+    }
   }
 }
