b78ada7c202ec5613e55b3e3ec196f3df56bb591
Ian Kirkpatrick <ikirkpat@oaisd.org>
2020-06-01 21:55:40 -0400

    Create frontend part

--- BudgetSquirrel.Api/ClientApp/src/app/budgets/add-budget-form/add-budget-form.component.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/budgets/add-budget-form/add-budget-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'bs-add-budget-form',
@@ -9,12 +10,34 @@ export class AddBudgetFormComponent implements OnInit {
 
   @Output() public onClose: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  public newBudgetForm: FormGroup;
+  nameValidation = new FormControl("", [Validators.required]);
+  fixedAmountValidation = new FormControl("", [Validators.required]);
+
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.newBudgetForm = this.formBuilder.group({
+      name: this.nameValidation,
+      fixedAmount: this.fixedAmountValidation
+    }, {
+      updateOn: "submit"
+    });
   }
 
   public onCloseClicked() {
     this.onClose.emit('close');
+  }
+
+  public onCreateBudgetSubmit() {
+    if (this.newBudgetForm.valid) {
+      const body = {
+        name: this.newBudgetForm.value.name,
+        fixedAmount: this.newBudgetForm.value.fixedAmount
+      };
+      console.log(body);
+      
+    }
   }
 }
--- BudgetSquirrel.Api/ClientApp/src/app/budgets/budgets.module.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/budgets/budgets.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { BudgetOverviewComponent } from "./budget-overview/budget-overview.component";
 import { SharedModule } from "../shared/shared.module";
-import { MatExpansionModule } from "@angular/material";
+import { MatExpansionModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule } from "@angular/material";
 import { BudgetComponent } from './budget/budget.component';
 import { AddBudgetFormComponent } from './add-budget-form/add-budget-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,8 +15,16 @@ import { AddBudgetFormComponent } from './add-budget-form/add-budget-form.compon
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     SharedModule,
-    MatExpansionModule
+    MatExpansionModule,
+
+    // Material
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     BudgetOverviewComponent,
