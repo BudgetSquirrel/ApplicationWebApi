303d0342d947c8e38ddf5bee08484137ba16c9b6
Ian Kirkpatrick <ikirkpat@oaisd.org>
2020-06-05 23:23:12 -0400

    Finish the feature. Need to add tests

--- BudgetSquirrel.Api/ClientApp/src/app/budgets/budget/budget.component.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/budgets/budget/budget.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Budget } from '../models';
 
 @Component({
   selector: 'bs-budget',
   template: `
-    <p>
-      budget works!
-    </p>
+    <div class="budget-item budget-item--level-{{this.level}}">
+      <div class="budget-item__name">
+        {{this.budget.name}}
+      </div>
+
+      <div class="budget-item__stats">
+        <span class="stat__container">
+          <span class="stat__label">
+            AMOUNT IN
+          </span>
+          <span class="stat">
+            {{this.budget.setAmount | currency}}
+          </span>
+        </span>
+
+        <span class="stat__container budget-item__balance-stat">
+          <span class="stat__label">
+            BALANCE
+          </span>
+          <span class="stat">
+            {{this.budget.fundBalance | currency}}
+          </span>
+        </span>
+      </div>
+    </div>
   `,
   styleUrls: ['./budget.component.scss']
 })
 export class BudgetComponent implements OnInit {
+
+  @Input() budget: Budget;
+  @Input() level: number;
 
   constructor() { }
 
