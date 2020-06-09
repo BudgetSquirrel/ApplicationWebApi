3f2463a83cb35fb59005b09a57e0a57298d5b4a3
Ian Kirkpatrick <ikirkpat@oaisd.org>
2020-06-09 07:51:22 -0400

    Style level 2 budgets

--- BudgetSquirrel.Api/ClientApp/src/app/budgets/budget/budget.component.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/budgets/budget/budget.component.ts
@@ -4,42 +4,53 @@ import { Budget } from '../models';
 @Component({
   selector: 'bs-budget',
   template: `
-    <div class="budget-item budget-item--level-{{this.level}}">
-      <div class="budget-item__actions">
-        <button (click)="onAddBudgetClicked()">
-          Haha
-        </button>
-      </div>
-
-      <div class="budget-item__content">
-        <p *ngFor="let subBudget of budget.subBudgets">
-          {{subBudget.name}}
-          {{subBudget.setAmount | currency}}
-        </p>
-        <div class="budget-item__name">
-          {{this.budget.name}}
+    <div class="budget-item__container budget-item--level-{{this.level}}">
+      <div class="budget-item">
+        <div class="budget-item__actions">
+          <button (click)="onAddBudgetClicked()">
+            Haha
+          </button>
         </div>
 
-        <div class="budget-item__stats">
-          <span class="stat__container">
-            <span class="stat__label">
-              AMOUNT IN
-            </span>
-            <span class="stat">
-              {{this.budget.setAmount | currency}}
-            </span>
-          </span>
+        <div class="budget-item__content">
+          <div class="budget-item__name">
+            {{this.budget.name}}
+          </div>
 
-          <span class="stat__container budget-item__balance-stat">
-            <span class="stat__label">
-              BALANCE
+          <div class="budget-item__stats">
+            <span class="stat__container">
+              <span class="stat__label">
+                AMOUNT IN
+              </span>
+              <span class="stat">
+                {{this.budget.setAmount | currency}}
+              </span>
             </span>
-            <span class="stat">
-              {{this.budget.fundBalance | currency}}
+
+            <span class="stat__container budget-item__balance-stat">
+              <span class="stat__label">
+                BALANCE
+              </span>
+              <span class="stat">
+                {{this.budget.fundBalance | currency}}
+              </span>
             </span>
-          </span>
+          </div>
         </div>
       </div>
+
+      <ng-template [ngIf]="this.hasSubBudgets" [ngIfElse]="hasNoSubBudgets">
+        <div class="budget-item__sub-budget-container">
+          <bs-budget
+            *ngFor="let subBudget of budget.subBudgets"
+            [budget]="subBudget"
+            [level]="subBudgetLevel"
+            (addBudget)="onAddBudgetClicked(subBudget)">
+          </bs-budget>
+        </div>
+      </ng-template>
+      <ng-template #hasNoSubBudgets>
+      </ng-template>
     </div>
   `,
   styleUrls: ['./budget.component.scss']
@@ -50,6 +61,16 @@ export class BudgetComponent implements OnInit {
   @Input() level: number;
 
   @Output() public addBudget: EventEmitter<any> = new EventEmitter();
+
+  get subBudgetLevel(): number {
+    return this.level + 1;
+  }
+
+  get hasSubBudgets(): boolean {
+    return !(this.budget.subBudgets === null ||
+           this.budget.subBudgets === undefined ||
+           this.budget.subBudgets.length === 0);
+  }
 
   constructor() { }
 
