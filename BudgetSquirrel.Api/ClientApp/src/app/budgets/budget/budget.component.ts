959d37ba594f4ce0e29b8094480d5f5df9d43889
Ian Kirkpatrick <ikirkpat@oaisd.org>
2020-07-02 21:35:56 -0400

    Add remove budget feature. Needs tests

--- BudgetSquirrel.Api/ClientApp/src/app/budgets/budget/budget.component.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/budgets/budget/budget.component.ts
@@ -10,88 +10,7 @@ export interface EditBudgetEvent {
 
 @Component({
   selector: 'bs-budget',
-  template: `
-    <div class="budget-item__container budget-item--level-{{this.level}}">
-      <div class="budget-item">
-        <div class="budget-item__content">
-          <div class="budget-item__header">
-            <ng-template [ngIf]="this.isEditingRootName" [ngIfElse]="notEditingRootName">
-              <input
-                class="inplace-input inplace-input__input inplace-input--large"
-                name="root-budget-name"
-                aria-label="Root Budget Name"
-                (blur)="onBlurInplaceEdit('rootName', $event)"
-                (mouseup)="$event.target.focus();" />
-            </ng-template>
-            <ng-template #notEditingRootName>
-              <h2
-                class="inplace-input inplace-input__focusable inplace-input--large"
-                (mousedown)="onOpenInplaceEdit('rootName', $event)">
-                {{this.budget.name}}
-              </h2>
-            </ng-template>
-
-            <ng-template [ngIf]="shouldShowAddBudgetButton" [ngIfElse]="noAddBudgetButton">
-              <div class="budget-item__actions">
-                <button class="button button--small button--primary" (click)="onAddBudgetClicked(this.budget)">
-                  Add SubBudget
-                </button>
-              </div>
-            </ng-template>
-            <ng-template #noAddBudgetButton>
-            </ng-template>
-          </div>
-
-          <div class="budget-item__stats">
-            <span class="stat__container">
-              <span class="stat__label">
-                {{this.amountInLabel}}
-              </span>
-              <ng-template [ngIf]="this.isEditingRootAmount" [ngIfElse]="notEditingRootAmount">
-                <input
-                  class="inplace-input inplace-input__input inplace-input--short stat stat--editing"
-                  type="number"
-                  step="0.01"
-                  aria-labelledby="rootAmountLabel"
-                  (blur)="onBlurInplaceEdit('rootAmount', $event)"
-                  (mouseup)="$event.target.focus();" />
-              </ng-template>
-              <ng-template #notEditingRootAmount>
-                <span
-                  class="inplace-input inplace-input--short inplace-input__focusable stat stat--editable"
-                  (mousedown)="onOpenInplaceEdit('rootAmount', $event)">
-                  {{this.budget.setAmount | currency}}
-                </span>
-              </ng-template>
-            </span>
-
-            <span class="stat__container budget-item__balance-stat">
-              <span class="stat__label">
-                {{this.balanceLabel}}
-              </span>
-              <span class="stat">
-                {{this.budget.fundBalance | currency}}
-              </span>
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <ng-template [ngIf]="this.hasSubBudgets" [ngIfElse]="hasNoSubBudgets">
-        <div class="budget-item__sub-budget-container">
-          <bs-budget
-            *ngFor="let subBudget of budget.subBudgets"
-            [budget]="subBudget"
-            [level]="subBudgetLevel"
-            (addBudget)="onAddBudgetClicked($event)"
-            (editBudget)="onEditBudget($event)">
-          </bs-budget>
-        </div>
-      </ng-template>
-      <ng-template #hasNoSubBudgets>
-      </ng-template>
-    </div>
-  `,
+  templateUrl: './budget.component.html',
   styleUrls: ['./budget.component.scss']
 })
 export class BudgetComponent implements OnInit {
@@ -101,6 +20,7 @@ export class BudgetComponent implements OnInit {
 
   @Output() public addBudget?: EventEmitter<any> = new EventEmitter();
   @Output() public editBudget?: EventEmitter<any> = new EventEmitter();
+  @Output() public removeBudget?: EventEmitter<any> = new EventEmitter();
 
   shouldShowAddBudgetButton: boolean;
   subBudgetLevel: number;
@@ -145,6 +65,10 @@ export class BudgetComponent implements OnInit {
     } else if (field === "rootName") {
       this.isEditingRootName = true;
     }
+  }
+
+  public onRemoveBudgetClicked(budget: Budget) {
+    this.removeBudget.emit(budget);
   }
 
   public onEditBudget(event: EditBudgetEvent) {
