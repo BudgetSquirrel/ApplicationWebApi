aba0a121526212efeef7dc50cb4278fd4202c3ce
Ian Kirkpatrick <ikirkpat@oaisd.org>
2020-08-16 17:15:44 -0400

    Add styling for duration type picker

--- BudgetSquirrel.Api/ClientApp/src/app/budgets/duration/edit-duration-form/edit-duration-form.component.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/budgets/duration/edit-duration-form/edit-duration-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { BudgetDuration, DurationType, Budget } from '../../models';
 import { BudgetService } from '../../services/budget.service';
+import { SwitchOption } from 'src/app/shared/components/switch/switch.component';
 
 export interface EditDurationEvent {
   endDayOfMonth?: number;
@@ -21,10 +22,17 @@ export class EditDurationFormComponent implements OnInit {
   @Output() public onEditSubmit: EventEmitter<EditDurationEvent> = new EventEmitter();
   @Output() public onClose?: EventEmitter<any> = new EventEmitter();
 
-  public durationTypes: DurationType[] = [
-    "MonthlyBookEnded",
-    "DaySpan"
+  public durationPickerOptions: SwitchOption<DurationType>[] = [
+    {
+      identifier: "MonthlyBookEnded",
+      text: "Ends on date"
+    },
+    {
+      identifier: "DaySpan",
+      text: "Spans number of days"
+    }
   ];
+  public activeDurationPickerOption: DurationType;
 
   public isMonthlyBookendedDuration: boolean;
   public isDaySpanDuration: boolean;
@@ -34,6 +42,7 @@ export class EditDurationFormComponent implements OnInit {
   ngOnInit() {
     this.isMonthlyBookendedDuration = this.budgetDuration.durationType == "MonthlyBookEnded";
     this.isDaySpanDuration = this.budgetDuration.durationType == "DaySpan";
+    this.activeDurationPickerOption = this.isDaySpanDuration ? "DaySpan" : "MonthlyBookEnded";
   }
 
   public onCloseClicked() {
@@ -44,13 +53,14 @@ export class EditDurationFormComponent implements OnInit {
     this.onEditSubmit.emit(input);
   }
 
-  public onChangeDurationType(type: DurationType) {
-    if (type == "DaySpan") {
+  public onChangeDurationType(option: SwitchOption<DurationType>) {
+    if (option.identifier == "DaySpan") {
       this.isDaySpanDuration = true;
       this.isMonthlyBookendedDuration = false;
     } else {
       this.isMonthlyBookendedDuration = true;
       this.isDaySpanDuration = false;
     }
+    this.activeDurationPickerOption = option.identifier;
   }
 }
--- BudgetSquirrel.Api/ClientApp/src/app/shared/components/switch/switch.component.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/shared/components/switch/switch.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit, Input, Output, EventEmitter, ContentChildren, TemplateRef } from '@angular/core';
 
+export interface SwitchOption<T> {
+  identifier: T;
+  text: string;
+};
+
 @Component({
   selector: 'bs-switch',
   template: `
     <div local-class="switch">
-      <button>
-        <button *ngFor="let child of templates">
-          <span *ngFor="let o of [child]" *ngForTemplate="child"></span>
-        </button>
+      <button
+        *ngFor="let option of this.optionValues"
+        class="switch__option"
+        [ngClass]="{'switch__option--active': option.identifier == this.activeOption}"
+        (click)="this.onSelectOption(option)">
+        {{option.text}}
       </button>
     </div>
   `,
@@ -15,18 +22,17 @@ import { Component, OnInit, Input, Output, EventEmitter, ContentChildren, Templa
 })
 export class SwitchComponent implements OnInit {
 
-  @Input() optionValues: string[];
+  @Input() optionValues: SwitchOption<any>[];
+  @Input() activeOption: any;
 
-  @Output() onSelect: EventEmitter<string> = new EventEmitter();
-
-  @ContentChildren(TemplateRef) templates;
+  @Output() onSelect: EventEmitter<SwitchOption<any>> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onSelectOption(option: string) {
+  onSelectOption(option: SwitchOption<any>) {
     this.onSelect.emit(option);
   }
 
