0917299bca84bdc2a14c13998e9cc1ec639a0970
Ben Groseclose <benjamin.groseclose@hotmail.com>
2020-09-23 20:04:50 -0400

    Starting Finalized Budget

--- BudgetSquirrel.Api/ResponseModels/CurrentBudgetPeriodResponse.cs
+++ BudgetSquirrel.Api/ResponseModels/CurrentBudgetPeriodResponse.cs
@@ -7,11 +7,13 @@ namespace BudgetSquirrel.Api.ResponseModels
     {
         public DateTime StartDate { get; private set; }
         public DateTime EndDate { get; private set; }
+        public DateTime? DateFinalized {get; private set;}
 
         public CurrentBudgetPeriodResponse(BudgetPeriod budgetPeriod)
         {
             this.StartDate = budgetPeriod.StartDate;
             this.EndDate = budgetPeriod.EndDate;
+            this.DateFinalized = budgetPeriod.DateFinalized;
         }
     }
 }
