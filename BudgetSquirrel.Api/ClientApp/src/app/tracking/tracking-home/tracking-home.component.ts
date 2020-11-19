984205cea904b1c7c4a2adf3d9138567900925cb
Ian Kirkpatrick <ikirkpat@oaisd.org>
2020-11-18 22:33:56 -0500

    Add initial display on tracking page

--- BudgetSquirrel.Api/ClientApp/src/app/tracking/tracking-home/tracking-home.component.ts
+++ BudgetSquirrel.Api/ClientApp/src/app/tracking/tracking-home/tracking-home.component.ts
@@ -4,11 +4,7 @@ import { TrackingApiService } from '../tracking-api.service';
 
 @Component({
   selector: 'bs-tracking-home',
-  template: `
-    <p>
-      {{this.rootFund?.name}}
-    </p>
-  `,
+  templateUrl: './tracking-home.component.html',
   styleUrls: ['./tracking-home.component.scss']
 })
 export class TrackingHomeComponent implements OnInit {
